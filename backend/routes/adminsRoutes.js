d5d7ec49767578a1f28e96b0546c054a53fb2432
Lazarte <fillip.k.lazarte@accenture.com>
2024-01-05 00:07:17 +0800

    Add edit functionality for news and updates as admin

--- backend/routes/adminsRoutes.js
+++ backend/routes/adminsRoutes.js
@@ -26,6 +26,7 @@ import {
     removeStudentFromSection,
     removeSubjectFromSection,
     createNewAndUpdates,
+    editNewsAndUpdates,
     getNewsAndUpdates,
     addStudentInSection,
     addSubjectInSection,
@@ -66,6 +67,7 @@ router.post('/update-student', updateStudentAccount)
 router.post('/remove-student', removeStudentFromSection)
 router.post('/remove-subject', removeSubjectFromSection)
 router.post('/create-news-updates', createNewAndUpdates)
+router.post('/edit-news-updates', editNewsAndUpdates)
 router.get('/get-news-updates', getNewsAndUpdates)
 router.post('/add-student-section', addStudentInSection)
 router.post('/add-subject-section', addSubjectInSection)
