020ad4a960f150641d16fb890b48585c1f76b95d
kairoslazarte <thelazarte@gmail.com>
2024-08-03 13:56:17 +0800

    test submit new message in student ui

--- frontend/src/components/student/dashboard/messages/StudentMessages.js
+++ frontend/src/components/student/dashboard/messages/StudentMessages.js
@@ -30,10 +30,12 @@ const StudentMessages = ({ studentDetails }) => {
     }
 
     const selectTeacherFormHandler = async (e) => {
+        console.log('i am here')
         e.preventDefault()
         try {
-            const { data } = await axios.post("/api/students/get-teacher", { teacherID: e.target.teacher.value })
-            setSelectedTeacher(data)
+            const { data } = await axios.get(`/api/messages/${e.target.teacher.value}`)
+            console.log(data)
+            setSelectedTeacher(e.target.teacher.value)
             setOnClickSendNewMsg(false)
         } catch (error) {
             console.log(error)
@@ -43,14 +45,11 @@ const StudentMessages = ({ studentDetails }) => {
     const sendMessageHandler = async(e) => {
         e.preventDefault()
         try {
-            const { data } = await axios.post("/api/students/send-message", {
-                studentID: student?._id,
-                teacherID: selectedTeacher?._id,
+            const { data } = await axios.post(`/api/messages/send/${selectedTeacher}`, {
                 message: e.target.message.value
             })
             setStudentMessage(data.studentMessage)
             setTeacherMessage(data.teacherMessage)
-            getStudentDetails()
         } catch (error) {
             console.log(error)
         }
@@ -97,7 +96,7 @@ const StudentMessages = ({ studentDetails }) => {
                             className="border border-blue-700 bg-blue-700 p-2 rounded-md group hover:bg-white transition duration-200"
                             onClick={() => setOnClickSendNewMsg(true, setSelectedTeacher(""))}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white group-hover:text-blue-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-white group-hover:text-blue-700">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                             </svg>
                         </button>
